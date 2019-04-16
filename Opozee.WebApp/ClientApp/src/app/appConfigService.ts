f531e098f1fb0ec913810cd3a59915bf3db453c9
vishrut <vishrut@gmail.com>
2019-04-15 22:36:49 -0400

    new icons and more cleanup (fixed margins on phone)

--- Opozee.WebApp/ClientApp/src/app/appConfigService.ts
+++ Opozee.WebApp/ClientApp/src/app/appConfigService.ts
@@ -4,6 +4,6 @@ import { Injectable } from '@angular/core';
 export class AppConfigService {
 
   public baseURL = "http://localhost:61545/";
-  //public baseURL = "http://3.19.56.18:81/"; 
-  //public baseURL = "http://prod.opozee.com:81/";
+  //public baseURL = "http://3.14.25.52:81/"; 
+  //public baseURL = "http://www.opozee.com:81/";
 }
