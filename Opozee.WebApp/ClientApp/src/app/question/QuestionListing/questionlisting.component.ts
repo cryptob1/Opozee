4c0fb45bd0486fd6e80e0e04ce4dcddaccf34154
vishrut malhotra <vishrut@gmail.com>
2019-06-02 11:50:18 -0400

    Revert "trying to fix infinite scroll"
    This reverts commit fc78b884f6eed64f5db28e78e2285cc4c5565857.

--- Opozee.WebApp/ClientApp/src/app/question/QuestionListing/questionlisting.component.ts
+++ Opozee.WebApp/ClientApp/src/app/question/QuestionListing/questionlisting.component.ts
@@ -35,8 +35,8 @@ export class QuestionListingComponent implements OnInit, OnDestroy {
   showSlider: boolean = false;
   shareUrl: any;
   sharetext: any;
-  //pager: any = {};
-  // pagedItems: any[];
+  pager: any = {};
+  pagedItems: any[];
   sliderData: PostQuestionDetail[] = [];
   bountyQuestions: any;
   startDate: any;
@@ -82,15 +82,14 @@ export class QuestionListingComponent implements OnInit, OnDestroy {
       this.initialize();
     });
 
-    this.questionGetModel.PageNumber = 1;// +localStorage.getItem('PageNumber');
+    this.questionGetModel.PageNumber = +localStorage.getItem('PageNumber');
 
     this.questionGetModel.Sort = +localStorage.getItem('Sort');
   }
 
   dropdownSort(kind: number) {
-    console.log(kind);
     this.isRecordLoaded = false;
-    //this.questionGetModel.PageNumber = localStorage.getItem('PageNumber') ? +localStorage.getItem('PageNumber') : 1;
+    this.questionGetModel.PageNumber = localStorage.getItem('PageNumber') ? +localStorage.getItem('PageNumber') : 1;
     this.questionGetModel.Sort = kind;
     localStorage.setItem('Sort', kind.toString());
     this.getAllQuestionlist(this.questionGetModel);
@@ -133,7 +132,7 @@ export class QuestionListingComponent implements OnInit, OnDestroy {
     }
     //this.questionGetModel.PageNumber = 1;
 
-    //this.questionGetModel.PageNumber = localStorage.getItem('PageNumber') ? +localStorage.getItem('PageNumber') : 1;
+    this.questionGetModel.PageNumber = localStorage.getItem('PageNumber') ? +localStorage.getItem('PageNumber') : 1;
 
     this.questionGetModel.TotalRecords = 5;
     this.questionGetModel.Sort = 0;
@@ -213,10 +212,10 @@ export class QuestionListingComponent implements OnInit, OnDestroy {
           //});
           //----------------------------------
           this.PercentageCalc(data);
-          //this.setPageonpageLoad(this.questionGetModel.PageNumber, this.questionGetModel.TotalRecords)
+          this.setPageonpageLoad(this.questionGetModel.PageNumber, this.questionGetModel.TotalRecords)
           this.isRecordLoaded = true
         } else {
-          //this.setPageonpageLoad(this.questionGetModel.PageNumber, this.questionGetModel.TotalRecords)
+          this.setPageonpageLoad(this.questionGetModel.PageNumber, this.questionGetModel.TotalRecords)
           this.isRecordLoaded = true
         }
 
@@ -300,113 +299,109 @@ export class QuestionListingComponent implements OnInit, OnDestroy {
     localStorage.setItem('PageNumber', PageNumber);
   }
 
-  //setPageonpageLoad(page, TotalRecords) {
-  //  this.pager = this.getPager(TotalRecords, page);
-  //}
+  setPageonpageLoad(page, TotalRecords) {
+    this.pager = this.getPager(TotalRecords, page);
+  }
 
-  //setPage(page, TotalRecords) {
+  setPage(page, TotalRecords) {
 
-  //  this.isRecordLoaded = false;
-  //  localStorage.setItem('PageNumber', page);
-  //  this.pager = this.getPager(this.questionGetModel.TotalRecords, page);
+    this.isRecordLoaded = false;
+    localStorage.setItem('PageNumber', page);
+    this.pager = this.getPager(this.questionGetModel.TotalRecords, page);
 
-  //  if (this.pager.totalPages >= page) {
-  //    this.PagingPagesload(this.pager.currentPage, this.pager.pageSize);
-  //  }
+    if (this.pager.totalPages >= page) {
+      this.PagingPagesload(this.pager.currentPage, this.pager.pageSize);
+    }
 
-  //  this.isRecordLoaded = true;
-  //  //window.scrollTo(0, 0);
-  //}
+    this.isRecordLoaded = true;
+    //window.scrollTo(0, 0);
+  }
 
-  //getPager(totalItems: number, currentPage: number = 1, pageSize: number = 10) {
-  //  // calculate total pages
+  getPager(totalItems: number, currentPage: number = 1, pageSize: number = 10) {
+    // calculate total pages
 
-  //  let totalPages = Math.ceil(totalItems / pageSize);
+    let totalPages = Math.ceil(totalItems / pageSize);
 
-  //  // ensure current page isn't out of range
-  //  if (currentPage < 1) {
-  //    currentPage = 1;
-  //  } else if (currentPage > totalPages) {
-  //    currentPage = totalPages;
-  //  }
+    // ensure current page isn't out of range
+    if (currentPage < 1) {
+      currentPage = 1;
+    } else if (currentPage > totalPages) {
+      currentPage = totalPages;
+    }
 
-  //  let startPage: number, endPage: number;
-  //  if (totalPages <= 10) {
-  //    // less than 10 total pages so show all
-  //    startPage = 1;
-  //    endPage = totalPages;
-  //  } else {
-  //    // more than 10 total pages so calculate start and end pages
-  //    if (currentPage <= 6) {
-  //      startPage = 1;
-  //      endPage = 10;
-  //    } else if (currentPage + 4 >= totalPages) {
-  //      startPage = totalPages - 9;
-  //      endPage = totalPages;
-  //    } else {
-  //      startPage = currentPage - 5;
-  //      endPage = currentPage + 4;
-  //    }
-  //  }
+    let startPage: number, endPage: number;
+    if (totalPages <= 10) {
+      // less than 10 total pages so show all
+      startPage = 1;
+      endPage = totalPages;
+    } else {
+      // more than 10 total pages so calculate start and end pages
+      if (currentPage <= 6) {
+        startPage = 1;
+        endPage = 10;
+      } else if (currentPage + 4 >= totalPages) {
+        startPage = totalPages - 9;
+        endPage = totalPages;
+      } else {
+        startPage = currentPage - 5;
+        endPage = currentPage + 4;
+      }
+    }
 
-  //  // calculate start and end item indexes
-  //  let startIndex = (currentPage - 1) * pageSize;
-  //  let endIndex = Math.min(startIndex + pageSize - 1, totalItems - 1);
+    // calculate start and end item indexes
+    let startIndex = (currentPage - 1) * pageSize;
+    let endIndex = Math.min(startIndex + pageSize - 1, totalItems - 1);
 
-  //  // create an array of pages to ng-repeat in the pager control
-  //  let pages = Array.from(Array((endPage + 1) - startPage).keys()).map(i => startPage + i);
+    // create an array of pages to ng-repeat in the pager control
+    let pages = Array.from(Array((endPage + 1) - startPage).keys()).map(i => startPage + i);
 
-  //  // return object with all pager properties required by the view
-  //  return {
-  //    totalItems: totalItems,
-  //    currentPage: currentPage,
-  //    pageSize: pageSize,
-  //    totalPages: totalPages,
-  //    startPage: startPage,
-  //    endPage: endPage,
-  //    startIndex: startIndex,
-  //    endIndex: endIndex,
-  //    pages: pages
-  //  };
-  //}
- 
-  onScroll(event) { 
+    // return object with all pager properties required by the view
+    return {
+      totalItems: totalItems,
+      currentPage: currentPage,
+      pageSize: pageSize,
+      totalPages: totalPages,
+      startPage: startPage,
+      endPage: endPage,
+      startIndex: startIndex,
+      endIndex: endIndex,
+      pages: pages
+    };
+  }
 
-      console.log("ssss");
-      console.log(event);
-      console.log(window.pageYOffset);
-      this.questionGetModel.PageNumber += 1;
-      this.questionGetModel.PageSize = 10;
+  onScroll() {
+    //console.log(new Date().getTime());
 
-      console.log(this.questionGetModel.PageNumber);
-      //debugger
-      this.userService.getAllQuestionlist(this.questionGetModel).subscribe(data => {
+    this.questionGetModel.PageNumber += 1;
+    this.questionGetModel.PageSize = 10;
+    //debugger
+    this.userService.getAllQuestionlist(this.questionGetModel).subscribe(data => {
 
-        if (data) {
-          if (data.length > 0) {
+      if (data) {
+        if (data.length > 0) {
 
-            for (var i = 0; i < data.length; i++) {
-              this.PostQuestionDetailList.push(data[i]);
-            }
-
-            this.questionGetModel.TotalRecords = data[0].TotalRecordcount
-
-            if (this.qid != -1) {
-              this.PostQuestionDetailList[0].comments = data[0]['Comments'];
-            }
-            //----------------------------------
-            this.PercentageCalc(data);
-            // this.setPageonpageLoad(this.questionGetModel.PageNumber, this.questionGetModel.TotalRecords)
-            //this.isRecordLoaded = true
-          } else {
-            // this.setPageonpageLoad(this.questionGetModel.PageNumber, this.questionGetModel.TotalRecords)
-            //this.isRecordLoaded = true
+          for (var i = 0; i < data.length; i++) {
+            this.PostQuestionDetailList.push(data[i]);
           }
 
+          this.questionGetModel.TotalRecords = data[0].TotalRecordcount
+
+          if (this.qid != -1) {
+            this.PostQuestionDetailList[0].comments = data[0]['Comments'];
+          }
+          //----------------------------------
+          this.PercentageCalc(data);
+          // this.setPageonpageLoad(this.questionGetModel.PageNumber, this.questionGetModel.TotalRecords)
+          //this.isRecordLoaded = true
+        } else {
+          // this.setPageonpageLoad(this.questionGetModel.PageNumber, this.questionGetModel.TotalRecords)
+          //this.isRecordLoaded = true
         }
-      }, error => {
-      });
-    
+
+      }
+    }, error => {
+    });
+
   }
 
 }
