d1fc0c73c46f24f28802da47f87d74b7771be093
vishrut malhotra <vishrut@gmail.com>
2019-06-30 21:50:35 -0400

    version updates

--- Opozee.WebApp/ClientApp/src/app/appConfigService.ts
+++ Opozee.WebApp/ClientApp/src/app/appConfigService.ts
@@ -6,7 +6,7 @@ export class AppConfigService {
   public bountyStartDate = '2019-05-23';
   public bountyEndDate = '2019-05-25';
   public coinPerReferral = 5;
-  public baseURL = "http://localhost:61545/";
+  //public baseURL = "http://localhost:61545/";
   //public baseURL = "http://3.18.212.192:81/"; 
-  //public baseURL = "https://opozee.com:81/";
+  public baseURL = "https://opozee.com:81/";
 }
