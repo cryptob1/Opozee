69c24b6dce1240eb7a0635518ddb8ecdf2fa7dea
vishrut malhotra <vishrut@gmail.com>
2019-07-28 13:43:31 -0400

    fix mixpanel

--- Opozee.WebApp/ClientApp/src/app/_services/mixpanel.service.ts
+++ Opozee.WebApp/ClientApp/src/app/_services/mixpanel.service.ts
@@ -13,7 +13,20 @@ export class MixpanelService {
   init(userToken: string): void {
     mixpanel.init('410cd4187cb6b07c074955910e753010');
     mixpanel.identify(userToken);
+    mixpanel.people.set({
+      "$email": userToken,    // only special properties need the $
+
+      //"$created": "2011-03-16 16:53:54",
+      //"$last_login": new Date(),         // properties can be dates...
+
+      //"credits": 150,                    // ...or numbers
+
+      //"gender": "Male"                    // feel free to define your own properties
+    });
   }
+
+
+
 
   /**
    * Push new action to mixpanel.
