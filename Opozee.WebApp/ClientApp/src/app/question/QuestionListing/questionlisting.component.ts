8d29b44e87bc7052074d4e0d1e951d6edec094a9
cryptob1 <vishrut@gmail.com>
2019-09-04 16:02:34 -0400

    fixing q

--- Opozee.WebApp/ClientApp/src/app/question/QuestionListing/questionlisting.component.ts
+++ Opozee.WebApp/ClientApp/src/app/question/QuestionListing/questionlisting.component.ts
@@ -228,6 +228,7 @@ export class QuestionListingComponent implements OnInit, OnDestroy {
           if (this.qid != -1) {
             this.PostQuestionDetailList[0].comments = data[0]['Comments'];
 
+
             this.meta.addTag({ name: 'description', content: data[0]['Question'] });
 
             this.meta.updateTag({ content: data[0]['Question'] }, 'name="description"');
@@ -329,6 +330,7 @@ export class QuestionListingComponent implements OnInit, OnDestroy {
    
 
   goToqDetail() {
+    
     this.router.navigate(['/questiondetail/', this.qid]);
   }
 
